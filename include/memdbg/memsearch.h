a18e7e3a62fcbf6eda71f3a0d7ea4b6bbc56b21c
andyscull.dev <andyscull.dev@2830a729-b440-e9c6-c81c-afcbe5f50f7b>
2014-10-20 05:52:03 +0000

    Fixed missing .cc files (my mistake, they were linked directly from VNR installation folder)

--- include/memdbg/memsearch.h
+++ include/memdbg/memsearch.h
@@ -33,7 +33,7 @@ dword_t findLastCallerAddress(dword_t funcAddr, dword_t funcInst, dword_t lowerB
 dword_t findLastCallerAddressAfterInt3(dword_t funcAddr, dword_t lowerBound, dword_t upperBound, dword_t callerSearchSize = MaximumFunctionSize);
 
 /**
- *  Return the absolute address of the call instruction address.
+ *  Return the absolute address of the long jump (not short jump) instruction address.
  *  The same as ITH FindCallOrJmpAbs(false).
  *
  *  @param  funcAddr  callee function address
@@ -41,10 +41,35 @@ dword_t findLastCallerAddressAfterInt3(dword_t funcAddr, dword_t lowerBound, dwo
  *  @param  upperBound  the upper memory address to search
  *  @return  the call instruction address if succeed or 0 if fail
  */
-dword_t findCallAddress(dword_t funcAddr, dword_t lowerBound, dword_t upperBound);
-
-///  Similar to findCallAddress except return jmp address
 dword_t findJumpAddress(dword_t funcAddr, dword_t lowerBound, dword_t upperBound);
+
+/**
+ *  Return the absolute address of the far call (inter-module) instruction address.
+ *  The same as ITH FindCallOrJmpAbs(true).
+ *
+ *  @param  funcAddr  callee function address
+ *  @param  lowerBound  the lower memory address to search
+ *  @param  upperBound  the upper memory address to search
+ *  @return  the call instruction address if succeed or 0 if fail
+ */
+dword_t findFarCallAddress(dword_t funcAddr, dword_t lowerBound, dword_t upperBound);
+
+///  Near call (intra-module)
+dword_t findNearCallAddress(dword_t funcAddr, dword_t lowerBound, dword_t upperBound);
+
+///  Default to far call
+inline dword_t findCallAddress(dword_t funcAddr, dword_t lowerBound, dword_t upperBound)
+{ return findFarCallAddress(funcAddr, lowerBound, upperBound); }
+
+///  Push value >= 0xff
+dword_t findPushDwordAddress(dword_t value, dword_t lowerBound, dword_t upperBound);
+
+///  Push value <= 0xff
+dword_t findPushByteAddress(byte_t value, dword_t lowerBound, dword_t upperBound);
+
+///  Default to push DWORD
+inline dword_t findPushAddress(dword_t value, dword_t lowerBound, dword_t upperBound)
+{ return findPushDwordAddress(value, lowerBound, upperBound); }
 
 /**
  *  Return the enclosing function address outside the given address.
@@ -57,39 +82,76 @@ dword_t findJumpAddress(dword_t funcAddr, dword_t lowerBound, dword_t upperBound
  *  @param  addr  address within th function
  *  @param  searchSize  max backward search size
  *  @return  beginning address of the function
+ *  @exception  illegal memory access
  */
 dword_t findEnclosingAlignedFunction(dword_t addr, dword_t searchSize = MaximumFunctionSize);
 
 /**
- *  Return the address of the first mached pattern.
- *  The same as ITH SearchPattern(). KMP is used.
+ *  Return the address of the first matched pattern.
  *  Return 0 if failed. The return result is ambiguous if the pattern address is 0.
+ *  This function simpily traverse all bytes in memory range and would raise
+ *  if no access to the region.
  *
- *  @param  startAddress  search start address
- *  @param  range  search range
  *  @param  pattern  array of bytes to match
  *  @param  patternSize  size of the pattern array
- *  @return
+ *  @param  lowerBound  search start address
+ *  @param  upperBound  search stop address
+ *  @return  absolute address
+ *  @exception  illegal memory access
  */
-dword_t searchPattern(dword_t startAddress, dword_t range, const void *pattern, dword_t patternSize);
+dword_t findBytes(const void *pattern, dword_t patternSize, dword_t lowerBound, dword_t upperBound);
+
+/**
+ *  jichi 2/5/2014: The same as findBytes except it uses widecard to match everything.
+ *  The widecard should use the byte seldom appears in the pattern.
+ *  See: http://sakuradite.com/topic/124
+ *
+ *  @param  pattern  array of bytes to match
+ *  @param  patternSize  size of the pattern array
+ *  @param  lowerBound  search start address
+ *  @param  upperBound  search stop address
+ *  @param* widecard  the character to match everything
+ *  @return  absolute address
+ *  @exception  illegal memory access
+ */
+enum : byte_t { WidecardByte = 0x11 }; // jichi 7/17/2014: 0x11 seldom appear in PSP code pattern
+//enum : WORD { WidecardWord = 0xffff };
+dword_t matchBytes(const void *pattern, dword_t patternSize, dword_t lowerBound, dword_t upperBound,
+                   byte_t wildcard = WidecardByte);
+
+// User space: 0 - 2G (0 - 0x7ffeffff)
+// Kernel space: 2G - 4G  (0x80000000 - 0xffffffff)
+//
+// http://msdn.microsoft.com/en-us/library/windows/hardware/ff560042%28v=vs.85%29.aspx
+// http://codesequoia.wordpress.com/2008/11/28/understand-process-address-space-usage/
+// http://stackoverflow.com/questions/17244912/open-process-with-debug-privileges-and-read-write-memory
+enum MemoryRange : dword_t {
+  UserMemoryStartAddress = 0, UserMemoryStopAddress = 0x7ffeffff
+  , KernelMemoryStartAddress = 0x80000000, KernelMemoryStopAddress = 0xffffffff
+  , MappedMemoryStartAddress = 0x01000000
+
+  , MemoryStartAddress = UserMemoryStartAddress, MemoryStopAddress = UserMemoryStopAddress
+};
 
 #if 0 // not used
-
 /**
- *  Search from stopAddres back to startAddress - range
- *  This function is not well debugged
+ *  Traverse memory continues pages and return the address of the first matched pattern.
+ *
+ *  @param  pattern  array of bytes to match
+ *  @param  patternSize  size of the pattern array
+ *  @param  lowerBound  search start address
+ *  @param  upperBound  search stop address
+ *  @param* search  search all pages (SearchAll) or stop on first illegal access (SearchFirst)
+ *  @return  absolute address
  */
-dword_t reverseSearchPattern(dword_t stopAddress, dword_t range, const void *pattern, dword_t patternSize);
+enum SearchType : byte_t { SearchAll = 0 , SearchFirst };
 
-/**
- * jichi 2/5/2014: The same as SearchPattern except it uses 0xff to match everything
- * According to @Andys, 0xff seldom appear in the source code: http://sakuradite.com/topic/124
- */
-enum : byte_t { SP_ANY = 0xff };
-//#define SP_ANY_2 SP_ANY,SP_ANY
-//#define SP_ANY_3 SP_ANY,SP_ANY,SP_ANY
-//#define SP_ANY_4 SP_ANY,SP_ANY,SP_ANY,SP_ANY
-dword_t searchPatternEx(dword_t startAddress, dword_t range, const void *pattern, dword_t patternSize, byte_t wildcard = SP_ANY);
+dword_t findBytesInPages(const void *pattern, dword_t patternSize,
+    dword_t lowerBound = MemoryStartAddress, dword_t upperBound = MemoryStopAddress,
+    SearchType search = SearchAll);
+dword_t matchBytesInPages(const void *pattern, dword_t patternSize,
+    dword_t lowerBound = MemoryStartAddress, dword_t upperBound = MemoryStopAddress,
+    byte_t wildcard = WidecardByte, SearchType search = SearchAll);
 
 #endif // 0
 
